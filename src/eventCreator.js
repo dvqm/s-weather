589c62f2d611060d82ddf8f73668b2f4b1eba4ed
Eliot Lasovy <10248779+dvqm@users.noreply.github.com>
2022-06-11 18:06:39 +0200

    eventCreator() add0, convertTime, setDataTime added.  setCurEvent() renamed to setWeather and restructured.

--- src/eventCreator.js
+++ src/eventCreator.js
@@ -1,4 +1,5 @@
 import weatherData from './weatherData';
+import names from './names.json';
 import ui from './ui.json';
 
 const eventCreator = () => {
@@ -6,93 +7,170 @@ const eventCreator = () => {
     const k2c = (temp) => {
       const kelvin = -273.16;
 
-      const celsium = Math.round((temp + kelvin) * 10) / 10;
+      const celsium = Math.round(temp + kelvin);
 
       return celsium;
+    };
+
+    const add0 = (date, prop) => {
+      const with0 = `0${date[prop]()}`.slice(-2);
+      return with0;
     };
 
     const convertTime = (seconds) => {
       const date = new Date(seconds * 1000);
 
-      const string = `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`;
+      const string = `
+      ${add0(date, 'getHours')} : 
+      ${add0(date, 'getMinutes')} : 
+      ${add0(date, 'getSeconds')}
+      `;
 
       return string;
     };
 
+    const setDateTime = () => {
+      const date = new Date();
+
+      const path = names.titles;
+
+      const dateSpan = node.querySelector(`#${path.date[0]}`);
+
+      const timeSpan = node.querySelector(`#${path.time[0]}`);
+
+      const mNames = [
+        'Jan',
+        'Feb',
+        'Mar',
+        'Apr',
+        'May',
+        'Jun',
+        'Jul',
+        'Aug',
+        'Sep',
+        'Oct',
+        'Nov',
+        'Dec',
+      ];
+
+      const days = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+      ];
+
+      dateSpan.textContent = `
+      ${date.getDate()} 
+      ${mNames[date.getMonth()]} 
+      ${date.getFullYear()}
+      `;
+
+      timeSpan.textContent = `
+      ${days[date.getDay()]} 
+      ${add0(date, 'getHours')} : 
+      ${add0(date, 'getMinutes')}
+      `;
+    };
+
     const city = node.querySelector('#cityInput');
 
-    city.addEventListener('keydown', (event) => {
-      if (event.keyCode === 13) {
-        const curTemp = ui.desktop.container.c.currentTemp.c;
+    const setWeather = async () => {
+      const name = names.titles;
 
-        const current = document.querySelector(`#${curTemp.current.id}`);
+      const skyImg = node.querySelector(`#${name.skyImg[0]}`);
 
-        const feelsLike = document.querySelector(`#${curTemp.feelsLike.id}`);
+      const sky = node.querySelector(`#${name.sky[0]}`);
 
-        const minimal = document.querySelector(`#${curTemp.minimal.id}`);
+      const current = node.querySelector(`#${name.current[0]}`);
 
-        const maximal = document.querySelector(`#${curTemp.maximal.id}`);
+      const feelsLike = node.querySelector(`#${name.feelsLike[0]}`);
 
-        const curData = ui.desktop.container.c.currentData.c;
+      const minimal = node.querySelector(`#${name.minimal[0]}`);
 
-        const humidity = document.querySelector(`#${curData.humidity.id}`);
+      const maximal = node.querySelector(`#${name.maximal[0]}`);
 
-        const pressure = document.querySelector(`#${curData.pressure.id}`);
+      const humidity = node.querySelector(`#${name.humidity[0]}`);
 
-        const windSpeed = document.querySelector(`#${curData.windSpeed.id}`);
+      const pressure = node.querySelector(`#${name.pressure[0]}`);
 
-        const otherData = ui.desktop.container.c.otherData.c;
+      const windSpeed = node.querySelector(`#${name.windSpeed[0]}`);
 
-        const sunrise = document.querySelector(
-          `#${otherData.sun.c.sunrise.c.time.id}`,
+      const deg = node.querySelector(`#${name.deg[0]}`);
+
+      const sunrise = node.querySelector(`#${name.sunrise[0]}`);
+
+      const sunset = node.querySelector(`#${name.sunset[0]}`);
+
+      try {
+        const data = await weatherData().getCurrent(city.value);
+
+        skyImg.src = data.weather.icon;
+
+        skyImg.alt = data.weather.description;
+
+        sky.textContent = data.weather.description;
+
+        current.textContent = k2c(data.main.temp);
+
+        const tempPath = ui.desktop.c.current.c;
+
+        const tempTexts = tempPath.tempWrapper.c.temperatures.c;
+
+        feelsLike.textContent = tempTexts.feelsLike.textContent.concat(
+          k2c(data.main.feels_like),
         );
 
-        const sunset = document.querySelector(
-          `#${otherData.sun.c.sunset.c.time.id}`,
+        minimal.textContent = tempTexts.minimal.textContent.concat(
+          k2c(data.main.temp_min),
         );
 
-        const setCurWeather = async () => {
-          const weather = await weatherData().getCurrent(city.value);
+        maximal.textContent = tempTexts.maximal.textContent.concat(
+          k2c(data.main.temp_max),
+        );
 
-          current.textContent = current.textContent.concat(
-            k2c(weather.main.temp),
-          );
+        const otherTexts = tempPath.otherData.c;
 
-          feelsLike.textContent = feelsLike.textContent.concat(
-            k2c(weather.main.feels_like),
-          );
+        humidity.textContent = otherTexts.humidity.textContent.concat(
+          data.main.humidity,
+          ' %',
+        );
 
-          minimal.textContent = minimal.textContent.concat(
-            k2c(weather.main.temp_min),
-          );
+        pressure.textContent = otherTexts.pressure.textContent.concat(
+          data.main.pressure,
+          ' hPa',
+        );
 
-          maximal.textContent = maximal.textContent.concat(
-            k2c(weather.main.temp_max),
-          );
+        windSpeed.textContent = otherTexts.windSpeed.textContent.concat(
+          data.wind.speed,
+          ' m/s',
+        );
+        deg.textContent = otherTexts.deg.textContent.concat(
+          data.wind.deg,
+          ' deg.',
+        );
 
-          humidity.textContent = humidity.textContent.concat(
-            weather.main.humidity,
-            ' %',
-          );
+        sunrise.textContent = convertTime(data.sys.sunrise);
 
-          pressure.textContent = pressure.textContent.concat(
-            weather.main.pressure,
-            ' hPa',
-          );
-
-          windSpeed.textContent = windSpeed.textContent.concat(
-            weather.wind.speed,
-            ' m/s',
-          );
-
-          sunrise.textContent = convertTime(weather.sys.sunrise);
-
-          sunset.textContent = convertTime(weather.sys.sunset);
-        };
-
-        setCurWeather();
+        sunset.textContent = convertTime(data.sys.sunset);
+      } catch (e) {
+        console.error(e);
       }
-    });
+    };
+
+    const action = (event) => {
+      if (event.keyCode === 13) {
+        setWeather();
+      }
+    };
+
+    city.addEventListener('keydown', action);
+
+    setWeather();
+    setDateTime();
 
     return node;
   };
