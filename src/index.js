8327efbaa26d82ea190f15fa0fee443701b2116e
Eliot Lasovy <10248779+dvqm@users.noreply.github.com>
2022-06-14 22:29:34 +0200

    index.js slider events added to ui.

--- src/index.js
+++ src/index.js
@@ -4,19 +4,34 @@ import uiCreator from './uiCreator';
 import './ui.scss';
 
 import contentCreator from './contentCreator';
+import eventCreator from './eventCreator';
 
 const app = async () => {
   const data = await weatherData('Szczecin');
 
-  const weather = JSON.parse(JSON.stringify(data));
+  const page = uiCreator(data);
 
-  const page = uiCreator();
-
-  const content = contentCreator(ui.desktop, weather);
+  const content = contentCreator(ui.desktop, data);
 
   const mainPage = content.mainPage(ui.desktop);
 
-  page.render(document.body, page.nodeCreate(mainPage));
+  const pageWithEvents = eventCreator(page.nodeCreate(mainPage));
+
+  pageWithEvents.slider(6, {
+    shell: 'hourlyCards',
+    card: 'card',
+    prev: 'hourlyPrev',
+    next: 'hourlyNext',
+  });
+
+  pageWithEvents.slider(4, {
+    shell: 'dailyCards',
+    card: 'card',
+    prev: 'dailyPrev',
+    next: 'dailyNext',
+  });
+
+  page.render(document.body, pageWithEvents.node);
 };
 
 app();
