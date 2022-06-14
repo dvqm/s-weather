de3492de62221560a2798fa688591852bf95da65
Eliot Lasovy <10248779+dvqm@users.noreply.github.com>
2022-06-14 02:19:26 +0200

    contentCreator - convertTime editing. hourlyCard(), dailyCard() finished.

--- src/contentCreator.js
+++ src/contentCreator.js
@@ -27,14 +27,22 @@ const contentCreator = (ui, data) => {
     return with0;
   };
 
-  const convertTime = (seconds) => {
+  const convertTime = (seconds, type) => {
     const date = new Date(seconds * 1000);
 
-    const string = `
-      ${add0(date, 'getHours')} : 
-      ${add0(date, 'getMinutes')} : 
-      ${add0(date, 'getSeconds')}
-      `;
+    const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+    let string = '';
+
+    if (type.includes('d')) string += `${add0(date, 'getDate')}`;
+
+    if (type.includes('w')) string += ` ${days[date.getDay()]}`;
+
+    if (type.includes('h')) string += `${add0(date, 'getHours')}`;
+
+    if (type.includes('m')) string += ` : ${add0(date, 'getMinutes')}`;
+
+    if (type.includes('s')) string += ` : ${add0(date, 'getSeconds')}`;
 
     return string;
   };
@@ -140,22 +148,156 @@ const contentCreator = (ui, data) => {
 
     const sun = getRef('sun', blank).c;
 
-    sun.sunrise.c.time.textContent = convertTime(data.sys.sunrise);
+    sun.sunrise.c.time.textContent = convertTime(data.sys.sunrise, 'hms');
 
-    sun.sunset.c.time.textContent = convertTime(data.sys.sunset);
+    sun.sunset.c.time.textContent = convertTime(data.sys.sunset, 'hms');
 
     return blank;
   };
 
   const hourlyCard = () => {
-    const hourly = ui.hourlyForecast;
+    const hourly = { ui: ui.hourlyForecast };
+
+    const f = data.forecast;
+
+    const card = getRef('hourlyCard', hourly);
+
+    const cards = hourly.ui.c.wrapper.c;
+
+    delete hourly.ui.c.wrapper.c.hourlyCard;
+
+    f.hourly.map((ticket, i) => {
+      const cardRef = structuredClone(card);
+
+      const key = `h${i + 1}`;
+
+      const hour = getRef('hour', cardRef);
+
+      hour.textContent = convertTime(ticket.dt, 'h');
+
+      const icon = getRef('icon', cardRef);
+
+      icon.src = ticket.weather[0].icon;
+
+      icon.alt = ticket.weather[0].description;
+
+      const descr = getRef('descr', cardRef);
+
+      descr.textContent = ticket.weather[0].main;
+
+      const temp = getRef('temperature', cardRef);
+
+      temp.innerHTML = `${k2c(ticket.temp)} ${temp.s}`;
+
+      const pressure = getRef('pressure', cardRef);
+
+      pressure.textContent = `${ticket.pressure} hPa`;
+
+      const humidity = getRef('humidity', cardRef);
+
+      humidity.textContent = `${ticket.humidity} %`;
+
+      const windSpeed = getRef('windSpeed', cardRef);
+
+      windSpeed.textContent = `${ticket.wind_speed} m/s`;
+
+      const deg = getRef('deg', cardRef);
+
+      deg.textContent = `${ticket.wind_deg} deg.`;
+
+      cards[key] = cardRef;
+
+      return cards[key];
+    });
 
     return hourly;
   };
 
   const dailyCard = () => {
-    const daily = ui.dailyForecast;
-    console.log(daily);
+    const daily = { ui: ui.dailyForecast };
+
+    const f = data.forecast;
+
+    const card = getRef('dailyCard', daily);
+
+    const cards = daily.ui.c.wrapper.c;
+
+    delete daily.ui.c.wrapper.c.dailyCard;
+
+    f.daily.map((ticket, i) => {
+      const cardRef = structuredClone(card);
+
+      const key = `d${i + 1}`;
+
+      const date = getRef('date', cardRef);
+
+      date.textContent = convertTime(ticket.dt, 'dw');
+
+      const icon = getRef('icon', cardRef);
+
+      icon.src = ticket.weather[0].icon;
+
+      icon.alt = ticket.weather[0].description;
+
+      const descr = getRef('descr', cardRef);
+
+      descr.textContent = ticket.weather[0].description;
+
+      const sunrise = getRef('sunrise', cardRef);
+
+      sunrise.textContent = convertTime(ticket.sunrise, 'hm');
+
+      const sunset = getRef('sunset', cardRef);
+
+      sunset.textContent = convertTime(ticket.sunset, 'hm');
+
+      const temperatures = getRef('temperatures', cardRef);
+
+      const temp = temperatures.c.temp.c;
+
+      temp.morning = k2c(ticket.temp.morn);
+
+      temp.day = k2c(ticket.temp.day);
+
+      temp.evening = k2c(ticket.temp.eve);
+
+      temp.night = k2c(ticket.temp.night);
+
+      const feels = temperatures.c.feelsLike.c;
+
+      feels.morning = k2c(ticket.feels_like.morn);
+
+      feels.day = k2c(ticket.feels_like.day);
+
+      feels.evening = k2c(ticket.feels_like.eve);
+
+      feels.night = k2c(ticket.feels_like.night);
+
+      const humidity = getRef('humidity', cardRef);
+
+      humidity.textContent = `${ticket.humidity} %`;
+
+      const pressure = getRef('pressure', cardRef);
+
+      pressure.textContent = `${ticket.pressure} hPa`;
+
+      const windSpeed = getRef('windSpeed', cardRef);
+
+      windSpeed.textContent = `${ticket.wind_speed} m/s`;
+
+      const deg = getRef('deg', cardRef);
+
+      deg.textContent = `${ticket.wind_deg} deg.`;
+
+      const uvi = getRef('uvi', cardRef);
+
+      uvi.textContent = ticket.uvi;
+
+      cards[key] = cardRef;
+
+      return cards[key];
+    });
+
     return daily;
   };
 
