fdb6e6656fa041fa1157aa65f6689135d3eac3a7
Eliot Lasovy <10248779+dvqm@users.noreply.github.com>
2022-12-15 02:17:03 -0700

    all data from index.js moved to the App.js. All magic is in the App.js

---
+++ src/App.js
@@ -0,0 +1,176 @@
+import { GetLocation, LookDuplicates } from './weatherData';
+import uiCreator from './uiCreator';
+import './ui.scss';
+
+import contentCreator from './contentCreator';
+import eventCreator from './eventCreator';
+
+const App = () => {
+  const uiCreate = uiCreator();
+
+  const uiRemove = (...markers) => {
+    [...markers].forEach((marker) => {
+      const element = document.querySelector(marker);
+
+      if (element) element.remove();
+    });
+  };
+
+  const loader = () => ({
+    add(text = 'Loading...') {
+      const model = {
+        tag: 'div',
+        className: 'loader',
+        c: {
+          phrase: {
+            tag: 'span',
+            textContent: text,
+          },
+        },
+      };
+      uiCreate.render(document.body, uiCreate.node(model));
+    },
+    remove() {
+      const element = document.querySelector('.loader');
+
+      if (element) element.remove();
+    },
+  });
+
+  const errorMessage = (text) => {
+    const error = contentCreator().errorMessage(text);
+
+    uiCreate.render(document.body, error);
+
+    setTimeout(() => {
+      error.remove();
+    }, 5000);
+  };
+
+  const forecastPage = (data) => {
+    loader().remove();
+
+    const content = contentCreator().forecastPage(data);
+
+    const events = eventCreator();
+
+    const dailySliderEvent = (node) => {
+      events.slider(node, {
+        offset: 6,
+        shell: 'hourlyCards',
+        card: 'card',
+        prev: 'hourlyPrev',
+        next: 'hourlyNext',
+      });
+    };
+
+    const hourlySliderEvent = (node) => {
+      events.slider(node, {
+        offset: 4,
+        shell: 'dailyCards',
+        card: 'card',
+        prev: 'dailyPrev',
+        next: 'dailyNext',
+      });
+    };
+
+    const cityInputEvent = (node) => {
+      events.cityInput(node, checkIfOneCity, {
+        inp: 'cityInput',
+        btn: 'submitBtn',
+      });
+    };
+
+    uiCreate.render(
+      document.body,
+      uiCreate.assemble(content)(
+        dailySliderEvent,
+        hourlySliderEvent,
+        cityInputEvent,
+      ),
+    );
+  };
+
+  const cityListDialog = (list) => {
+    const content = contentCreator().citiesList(list);
+
+    const events = eventCreator();
+
+    const forecast = async (city) => {
+      const data = await GetLocation(city);
+
+      uiRemove('.city', '.container');
+
+      loader().add();
+
+      forecastPage(data);
+    };
+
+    uiCreate.render(document.body, events.citiesList(content, list, forecast));
+  };
+
+  const checkIfOneCity = async (city) => {
+    loader().remove();
+
+    const data = await LookDuplicates(city);
+    if (data.length === 0) {
+      errorMessage('No such city found. Please, check the name.');
+    } else if (data.length === 1) {
+      const forecast = await GetLocation(data[0]);
+
+      uiRemove('.city', '.container');
+
+      forecastPage(forecast);
+    } else {
+      cityListDialog(data);
+    }
+  };
+
+  const getLocationFromBrowser = async (location) => {
+    loader().remove();
+
+    loader().add();
+
+    const coords = {
+      lon: location.coords.longitude,
+      lat: location.coords.latitude,
+    };
+
+    const forecast = await GetLocation(coords);
+
+    forecastPage(forecast);
+  };
+
+  const manualInputPage = (error) => {
+    loader().remove();
+
+    errorMessage(error.message);
+
+    const content = contentCreator().manualInput();
+
+    const events = eventCreator();
+
+    uiCreate.render(
+      document.body,
+      events.cityInput(content, checkIfOneCity, {
+        inp: 'cityInput',
+        btn: 'submitBtn',
+      }),
+    );
+  };
+
+  return {
+    start() {
+      loader().add(
+        'Retreiving location. Please, allow it in your browser settings',
+      );
+
+      navigator.geolocation.getCurrentPosition(
+        getLocationFromBrowser,
+        manualInputPage,
+      );
+    },
+  };
+};
+
+export default App;
