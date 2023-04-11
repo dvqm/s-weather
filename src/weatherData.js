508a89136f3c8cf73652004b0bc52a05637846f8
Eliot Lasovy <10248779+dvqm@users.noreply.github.com>
2023-04-10 21:55:07 -0600

    updating weatherData regarding to the new data (40 items per forecast)

--- src/weatherData.js
+++ src/weatherData.js
@@ -3,6 +3,7 @@ const URL_DATA_V25 = 'https://api.openweathermap.org/data/2.5';
 const URL_GEO_V10 = 'https://api.openweathermap.org/geo/1.0';
 
 export const LookDuplicates = async (cityName) => {
+  try {
   const request = await fetch(
     `${URL_GEO_V10}/direct?q=${cityName}&limit=10&appid=${process.env.WEATHER_API_KEY}`,
     { mode: 'cors' },
@@ -11,44 +12,62 @@ export const LookDuplicates = async (cityName) => {
   const response = await request.json();
 
   return response;
+  } catch (error) {
+    alert(error.message);
+    console.warn(error.message);
+    return error;
+  }
 };
 
 export const GetLocation = async (cityData) => {
-  const request = await fetch(
-    `${URL_GEO_V10}/reverse?lat=${cityData.lat}&lon=${cityData.lon}&appid=${process.env.WEATHER_API_KEY}`,
-    { mode: 'cors' },
-  );
+  try {
+    const request = await fetch(
+      `${URL_GEO_V10}/reverse?lat=${cityData.lat}&lon=${cityData.lon}&appid=${process.env.WEATHER_API_KEY}`,
+      { mode: 'cors' },
+    );
 
-  const response = await request.json();
+    const response = await request.json();
 
-  const forecastRequest = await fetch(
-    `${URL_DATA_V25}/onecall?lat=${cityData.lat}&lon=${cityData.lon}&exclude=minutely&appid=${process.env.WEATHER_API_KEY}`,
-    { mode: 'cors' },
-  );
+    const forecastRequest = await fetch(
+      `${URL_DATA_V25}/forecast?lat=${cityData.lat}&lon=${cityData.lon}&appid=${process.env.WEATHER_API_KEY}`,
+      { mode: 'cors' },
+    );
 
-  const forecastResponse = await forecastRequest.json();
+    const forecastResponse = await forecastRequest.json();
 
-  const data = {};
+    const weatherRequest = await fetch(
+      `${URL_DATA_V25}/weather?lat=${cityData.lat}&lon=${cityData.lon}&appid=${process.env.WEATHER_API_KEY}`,
+      { mode: 'cors' },
+    )
 
-  [data.common] = response;
+    const weatherResponse = await weatherRequest.json();
 
-  data.forecast = forecastResponse;
+    const data = {};
 
-  const weather = forecastResponse.current.weather[0];
+    [data.common] = response;
 
-  data.forecast.current.weather[0].icon = `http://openweathermap.org/img/wn/${weather.icon}@2x.png`;
+    data.forecast = forecastResponse;
 
-  const setUrl = (array) => {
-    array.forEach((card) => {
-      const cond = card.weather[0];
+    data.weather = weatherResponse;
 
-      cond.icon = `http://openweathermap.org/img/wn/${card.weather[0].icon}@2x.png`;
-    });
-  };
+    const weather = weatherResponse.weather[0];
 
-  setUrl(data.forecast.hourly);
+    data.weather.weather[0].icon = `http://openweathermap.org/img/wn/${weather.icon}@2x.png`;
 
-  setUrl(data.forecast.daily);
+    const setUrl = (array) => {
+      array.forEach((card) => {
+        const cond = card.weather[0];
 
-  return data;
+        cond.icon = `http://openweathermap.org/img/wn/${card.weather[0].icon}@2x.png`;
+      });
+    };
+
+    setUrl(data.forecast.list);
+
+    return data;
+  } catch (error) {
+    alert(error.message);
+    console.warn(error.message);
+    return error.message;
+  }
 };
